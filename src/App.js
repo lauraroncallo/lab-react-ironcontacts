d6e91a8a7df4018ab4afc4ea6023fc1c2c6290c8
Laura Roncallo <lrlafontt@gmail.com>
2021-09-21 14:50:56 +0100

    solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import contactsJSON from "./contacts.json";
+import React, {useState} from "react";
 
 function App() {
+
+  const [contacts, setContacts] = useState(contactsJSON.slice(0,5));
+
+  const addRandomContact=() => {
+  const index = Math.floor(Math.random() * contactsJSON.length);
+  const newRandomContact = contactsJSON[index];
+  contactsJSON.slice(index,1);
+  setContacts([newRandomContact,...contacts]);
+  };
+
+  const sortByPopularity=()=>{
+ contacts.sort((a,b)=> (b.popularity - a.popularity));
+  setContacts([...contacts]);
+ 
+  };
+
+  const sortByName=()=>{
+    contacts.sort((a,b)=> a.name.localeCompare(b.name));
+    setContacts([...contacts]);
+   
+    };
+
+    const deleteContact = (id) => {
+     setContacts(contacts.filter((contact) => contact.id !== id));
+    };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<>
+<div className="App">
+<table>
+<tr>
+<th><h2>Picture</h2></th>
+ <th><h2>Name</h2></th>
+<th><h2>Popularity</h2></th>
+<th><h2>Won Oscar</h2></th>
+<th><h2>Won Emmy</h2></th>
+<th><h2>Actions</h2></th>
+</tr>
+{contacts.map((contact) => {
+            return (
+                <tr>
+<th><img src={contact.pictureUrl} width="200" height="200"/> </th>
+                  <th>{contact.name}</th>
+                  <th>{contact.popularity}</th>
+                  <th>{contact.wonOscar ? "🏆" : "" }</th>
+                  <th>{contact.wonEmmy ? "🏆" : ""}</th>
+                  <th> <button onClick={() => deleteContact(contact.id)}>Delete</button></th>
+                </tr>
+            )
+          })}
+</table>
+<button onClick={addRandomContact}>Add random contact</button>
+<button onClick={sortByPopularity}>Sort by popularity</button>
+<button onClick={sortByName}>Sort by Name</button>
+</div> 
+</>  
+  )   
 }
 
 export default App;
